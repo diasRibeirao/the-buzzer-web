342d87330087384e347bfa61d88970a1b8d51c45
Caio Benatti <caionta@gmail.com>
2021-09-24 19:50:34 -0300

    erros

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import * as React from 'react';
 import logobuser from './buser.png';
+import { Button} from 'react-bootstrap';
 
 function Home() {
   return (
     <><div className='home'>
       <h1>Login</h1>
     </div><div className='footers'>
-        <img src={buser} />
+        <img src={logobuser} />
         <div className='buttons'>
-          <Button className="button" color="error" variant="contained">Facebook</Button>
-          <Button className="button" color="error" variant="contained">Google</Button>
+          <Button className="button" color="error" variant="contained">Login</Button>
+          <Button className="button" color="error" variant="contained">Cadastrar</Button>
         </div>
       </div></>
 
