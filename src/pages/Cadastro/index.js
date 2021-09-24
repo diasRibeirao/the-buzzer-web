f749d7c5c5ff02ff701d6f76717a90e49086fb73
Caio Benatti <caionta@gmail.com>
2021-09-24 17:50:23 -0300

    Telas caio

---
+++ src/pages/Cadastro/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <><div className='home'>
+      <h1>Cadastro</h1>
+    </div><div className='footers'>
+    <form onSubmit={handleSubmit(data => saveData(data))}>
+     <label>Nome</label>
+     <input name="nome" ref={register} />
+     <label>E-mail</label>
+     <input name="senha" ref={register} />    
+     <label>Confirmação E-mail</label>
+     <input name="nome" ref={register} />
+     <label>Senha</label>
+     <input name="senha" ref={register} />
+     <label>Confirmar Senha</label>
+     <input name="nome" ref={register} />
+     <input type="submit"  /> 
+   </form>
+
+      </div></>
+
+  );
+}
+
+export default Home;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import logobuser from './buser.png';
 
 function Home() {
   return (
-    <div className='home'>
-      <h1>Home</h1>
-    </div>
+    <><div className='home'>
+      <h1>Login</h1>
+    </div><div className='footers'>
+        <img src={buser} />
+        <div className='buttons'>
+          <Button className="button" color="error" variant="contained">Facebook</Button>
+          <Button className="button" color="error" variant="contained">Google</Button>
+        </div>
+      </div></>
+
   );
 }
 
