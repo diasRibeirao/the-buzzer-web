4d9a35af2afb310927452099d7260814966b2af7
Caio Benatti <caionta@gmail.com>
2021-09-24 22:00:33 -0300

    versão final

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import './styles.css';
+import Button from '@restart/ui/esm/Button';
+import './styles.css';
+import { useHistory } from "react-router-dom"; 
 
 function Cadastro() {
+  const history = useHistory();
+  const handleNavigateToDirect = () => history.push("/");
   return (
     <><div className='home'>
       <h1>Cadastro</h1>
     </div><div className='footers'>
     <form >
-     <label>Nome</label>
-     <input name="nome"  />
-     <label>E-mail</label>
-     <input name="senha"  />    
-     <label>Confirmação E-mail</label>
-     <input name="nome" />
+      <div>
+     <label>Nome</label> </div>
+     <div>
+     <input name="nome"  /> </div>
+     <div> <label>E-mail</label> </div>
+     <div> <input name="senha"  /> </div>    
+     <div>  <label>Confirmação E-mail</label> </div>
+     <div> <input name="nome" /> </div>
      <label>Senha</label>
-     <input name="senha"  />
-     <label>Confirmar Senha</label>
-     <input name="nome" />
-     <input type="submit"  /> 
+     <div> <input name="senha"  /> </div>
+     <div> <label>Confirmar Senha</label> </div>
+     <div><input name="nome" /> </div>
+     <Button className='btnLoginRealizar' onClick={handleNavigateToDirect}>Realizar Cadastro</Button>
+       
    </form>
 
       </div></>
--- src/pages/Esqueceu/index.js
+++ src/pages/Esqueceu/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import './styles.css';
+import Button from '@restart/ui/esm/Button';
+import { useHistory } from "react-router-dom"; 
 
 function EsqueceuSenha() {
+  const history = useHistory();
+  const handleNavigateToDirect = () => history.push("/");
   return (
     <><div className='home'>
-      <h1>Por favor, informe abaixo seu e-mail de cadastro.</h1>
+      <h4>Por favor, informe abaixo seu e-mail de cadastro.</h4>
     </div><div className='footers'>
     <form>
      <label>E-mail</label>
-     <input name="senha"/>    
-     <input type="submit"  /> 
+     <div><input name="senha"/></div>    
+     <Button className='btnLoginRealizar' onClick={handleNavigateToDirect}>Enviar</Button>
+       
    </form>
 
       </div></>
